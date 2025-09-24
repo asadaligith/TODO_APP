3bcc93ad6cd8bc036fb1666e50b3da200d28097e
asadaligith <asad2747982@gmail.com>
2025-09-24 05:07:56 +0500

    Todo Updated

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 
 function todo_func(){
+    //  target to input & Value
     var todo_input = document.getElementById("input")
     var todo_value = todo_input.value 
     console.log(todo_value)
 
-    if (todo_value.trim() == ""){
-        alert("Enter a Task")
-    }
+    // Checking Empty Task
+    if (todo_value.trim() !== ""){
+        // Creating Tag <li> </li>
+    var list_tag = document.createElement("li")
 
-    var list = document.createElement("li")
+    // Creating textNode : Received From Input Data
     var list_value = document.createTextNode(todo_value)
-    list.appendChild(list_value)
-    console.log(list)
 
-    var todo_list_item = document.getElementById("todo-li")
-    todo_list_item.appendChild(list)
+    // textNode input data append into <li> here puting</li>
+    list_tag.appendChild(list_value)
+    console.log(list_tag)
+
+    // UnOrdered List Target into HTML
+    var todo_ul = document.getElementById("todo-ul") 
+    todo_ul.appendChild(list_tag)
     todo_input.value = ""
 
 
+// Delete Task from Unordered List 
+    var delete_btn = document.createElement("button")
+    var dlt_txt = document.createTextNode("Delete")
+    delete_btn.appendChild(dlt_txt)
+    console.log(delete_btn)
+    list_tag.appendChild(delete_btn)
+    delete_btn.setAttribute("class" , "delete-btn")
+
+    delete_btn.addEventListener("click", function(){
+    todo_ul.removeChild(list_tag)
+    })
+    }
+    else {
+        alert("Enter a Task")
+    }
 }
